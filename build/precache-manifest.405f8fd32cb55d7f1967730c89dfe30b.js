2d8abaa0d559655e42f35f98db53b899bfd3ce1a
Rainer2465 <rainerdaniel9@hotmail.com>
2020-02-10 02:13:13 -0300

    Add files via upload

---
+++ build/precache-manifest.405f8fd32cb55d7f1967730c89dfe30b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583b1a1acc2331831274d4ccbb6dbbf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f76d8db0f113e1149f46",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "79d6988991609473a51c",
+    "url": "/static/css/main.e538441f.chunk.css"
+  },
+  {
+    "revision": "f76d8db0f113e1149f46",
+    "url": "/static/js/2.4a99eb2a.chunk.js"
+  },
+  {
+    "revision": "79d6988991609473a51c",
+    "url": "/static/js/main.04af9612.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
